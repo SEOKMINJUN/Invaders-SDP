63e3273f62f52f1be48547503bc8014adec41d68
DongjunSuh923 <1dongjun26@gmail.com>
2024-12-02 13:32:32 +0900

    -Add one more test to AchievementManagerTest

--- test/achievements/AchievementManagerTest.java
+++ test/achievements/AchievementManagerTest.java
@@ -3,11 +3,14 @@ package achievements;
 import engine.Achievement.Achievement;
 import engine.Achievement.AchievementManager;
 import engine.Achievement.AchievementType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class AchievementManagerTest {
 
@@ -21,10 +24,23 @@ class AchievementManagerTest {
     }
 
     @Test
+    @DisplayName("Test if achievements are added to achievementMap correctly")
+    void testAddAchievementToMap() {
+        Achievement achievement = new Achievement("Kill 50 Enemies", "Kill 50 enemies in total", 50, AchievementType.KILLS);
+        achievementManager.addAchievement(achievement);
+
+        List<Achievement> killsAchievements = achievementManager.achievementMap.get(AchievementType.KILLS);
+        assertNotNull(killsAchievements, "Achievement map should contain KILLS type.");
+        assertTrue(killsAchievements.contains(achievement), "Achievement should be added to the map under KILLS type.");
+    }
+
+    @Test
     void testCheckAchievementUnlock() {
         achievementManager.checkAchievement(AchievementType.KILLS, 15);
         Achievement testAchievement = achievementManager.achievementMap.get(AchievementType.KILLS).get(0);
+        System.out.println("Achievement state: " + testAchievement);
         assertTrue(testAchievement.isCompleted());
+        System.out.println("Achievement state: " + testAchievement);
     }
 
     @Test
